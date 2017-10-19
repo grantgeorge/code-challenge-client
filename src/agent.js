dd42e3ae966959c67eff88bd7858b4b4602e9978
Grant George <grant@grantgeorge.io>
2017-10-19 14:54:37 -0700

    init

--- src/agent.js
+++ src/agent.js
@@ -3,8 +3,8 @@ import _superagent from 'superagent'
 
 const superagent = superagentPromise(_superagent, global.Promise)
 
-// const API_ROOT = 'https://sts-code-challenge.herokuapp.com/api'
-const API_ROOT = 'http://localhost:1337/api'
+const API_ROOT = 'https://sts-code-challenge.herokuapp.com/api'
+// const API_ROOT = 'http://localhost:1337/api'
 
 const encode = encodeURIComponent
 const responseBody = res => res.body
